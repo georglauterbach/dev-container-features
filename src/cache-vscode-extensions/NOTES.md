21cf9c00fe148a90f7122a56e52db8c114648546
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-03-30 13:56:20 +0200

    feat: new extension for caching VS code extensions
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

---
+++ src/cache-vscode-extensions/NOTES.md
@@ -0,0 +1,3 @@
+> [!NOTE]
+>
+> This extension is based upon <https://github.com/microsoft/vscode-remote-release/issues/7690#issuecomment-2761197753> by @chrmarti.
---
+++ src/cache-vscode-extensions/install.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+# shellcheck disable=SC2154
+
+set -eE -u -o pipefail
+shopt -s inherit_errexit
+
+readonly CACHE_MOUNT_POINT='/opt/vscode-extensions-cache'
+
+mkdir --parents "${CACHE_MOUNT_POINT}/"{stable,insiders}
+chown --recursive "${_REMOTE_USER}:${_REMOTE_USER}" "${CACHE_MOUNT_POINT}"
+
+mkdir --parents "${_REMOTE_USER_HOME}/.vscode-server"{,-insiders}
+chown "${_REMOTE_USER}:${_REMOTE_USER}" "${_REMOTE_USER_HOME}/.vscode-server"{,-insiders}
+
+ln --symbolic --no-target-directory \
+  "${CACHE_MOUNT_POINT}/stable"   "${_REMOTE_USER_HOME}/.vscode-server/extensions"
+ln --symbolic --no-target-directory \
+  "${CACHE_MOUNT_POINT}/insiders" "${_REMOTE_USER_HOME}/.vscode-server-insiders/extensions"
+
+chown --no-dereference "${_REMOTE_USER}:${_REMOTE_USER}" \
+  "${_REMOTE_USER_HOME}/.vscode-server/extensions"       \
+  "${_REMOTE_USER_HOME}/.vscode-server-insiders/extensions"
