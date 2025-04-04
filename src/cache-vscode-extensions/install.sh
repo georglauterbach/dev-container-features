39d3d40a869fa10262856bedac72e0317f82759d
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-04-04 16:10:36 +0200

    fix: back to working state

--- src/cache-vscode-extensions/install.sh
+++ src/cache-vscode-extensions/install.sh
@@ -8,10 +8,13 @@ shopt -s inherit_errexit
 readonly CACHE_MOUNT_POINT='/opt/vscode-extensions-cache'
 
 mkdir --parents "${CACHE_MOUNT_POINT}/"{stable,insiders}
-#chown --recursive "${_REMOTE_USER}:${_REMOTE_USER}" "${CACHE_MOUNT_POINT}"
+chown --recursive "${_REMOTE_USER}:${_REMOTE_USER}" "${CACHE_MOUNT_POINT}"
+chmod --recursive 777 "${CACHE_MOUNT_POINT}"
 
 mkdir --parents "${_REMOTE_USER_HOME}/.vscode-server"{,-insiders}
-chown --recursive "${_REMOTE_USER}:${_REMOTE_USER}" "${_REMOTE_USER_HOME}/.vscode-server"{,-insiders}
+chmod --recursive 777 \
+  "${_REMOTE_USER}:${_REMOTE_USER}" \
+  "${_REMOTE_USER_HOME}/.vscode-server"{,-insiders}
 
 readonly EXTENSIONS_DIR="${_REMOTE_USER_HOME}/.vscode-server/extensions"
 [[ -e ${EXTENSIONS_DIR} ]] && rm --recursive --force "${EXTENSIONS_DIR}"
