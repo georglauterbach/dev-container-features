16b5eda8448a17aed3eae75eb04d20c4e5e11b32
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-04-04 16:55:36 +0200

    feat: simplify `install.sh`

--- src/cache-vscode-extensions/install.sh
+++ src/cache-vscode-extensions/install.sh
@@ -5,21 +5,23 @@
 set -eE -u -o pipefail
 shopt -s inherit_errexit
 
-readonly CACHE_MOUNT_POINT='/opt/vscode-extensions-cache'
+# This is where the volume will be mounted to
+readonly CACHE_MOUNT_POINT='/opt/vscode/extensions/cache'
 
-mkdir --parents "${CACHE_MOUNT_POINT}/"{stable,insiders}
-chown --recursive "${_CONTAINER_USER}:${_REMOTE_USER}" "${CACHE_MOUNT_POINT}"
-chmod --recursive 777 "${CACHE_MOUNT_POINT}"
+# We create the mount point and temporary directories here with the
+# correct permissions. This is especially imperative for the mount
+# point as the volume mount will cary over the _permissions_ of an
+# existing directory. We _cannot_ use `chown` here because of
+# `updateRemoteUserUID: true` in some cases, which would result in
+# a UID mismatch; hence, we need `777` as the permissions.
+for CACHE_DIR in "stable," "insiders,-insiders"; do
+  PERSISTENCE_DIR="${CACHE_MOUNT_POINT}/$(cut --delimiter=, --fields=1)"
+  TMP_STORAGE_DIR="${_REMOTE_USER_HOME}/.vscode-server$(cut --delimiter=, --fields=2)"
 
-mkdir --parents "${_REMOTE_USER_HOME}/.vscode-server"{,-insiders}
-chmod --recursive 777 "${_REMOTE_USER_HOME}/.vscode-server"{,-insiders}
+  mkdir --parents "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
+  chmod --recursive 777 "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
 
-readonly EXTENSIONS_DIR="${_REMOTE_USER_HOME}/.vscode-server/extensions"
-[[ -e ${EXTENSIONS_DIR} ]] && rm --recursive --force "${EXTENSIONS_DIR}"
-ln --force --symbolic --no-target-directory "${CACHE_MOUNT_POINT}/stable" "${EXTENSIONS_DIR}"
-
-readonly EXTENSIONS_INSIDERS_DIR="${_REMOTE_USER_HOME}/.vscode-server-insiders/extensions"
-[[ -e ${EXTENSIONS_INSIDERS_DIR} ]] && rm --recursive --force "${EXTENSIONS_INSIDERS_DIR}"
-ln --force --symbolic --no-target-directory "${CACHE_MOUNT_POINT}/insiders" "${EXTENSIONS_INSIDERS_DIR}"
-
-chmod --recursive 777 "${EXTENSIONS_DIR}" "${EXTENSIONS_INSIDERS_DIR}"
+  TMP_STORAGE_DIR="${TMP_STORAGE_DIR}/extensions"
+  rm --recursive --force "${TMP_STORAGE_DIR}"
+  ln --force --symbolic "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
+done
