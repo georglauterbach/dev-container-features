a76ee754fdc41347ba0e5d55a49c78ef35308db2
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-04-09 11:39:16 +0200

    chore: changed mount point for cache to more uniform dir
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- src/cache-vscode-extensions/install.sh
+++ src/cache-vscode-extensions/install.sh
@@ -6,7 +6,7 @@ set -eE -u -o pipefail
 shopt -s inherit_errexit
 
 # This is where the volume will be mounted to
-readonly CACHE_MOUNT_POINT='/opt/vscode/extensions/cache'
+readonly CACHE_MOUNT_POINT='/opt/devcontainer/features/ghcr_io/georglauterbach/cache_vscode_extensions/data'
 
 # We create the mount point and temporary directories here with the
 # correct permissions. This is especially imperative for the mount
@@ -18,12 +18,12 @@ for LOOP_VAR in "stable," "insiders,-insiders"; do
   PERSISTENCE_DIR="${CACHE_MOUNT_POINT}/$(\
     cut --delimiter=, --fields=1 <<< "${LOOP_VAR}")"
   TMP_STORAGE_DIR="${_REMOTE_USER_HOME}/.vscode-server$(\
-    cut --delimiter=, --fields=2  <<< "${LOOP_VAR}")"
+    cut --delimiter=, --fields=2 <<< "${LOOP_VAR}")"
 
   mkdir --parents "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
   chmod --recursive 777 "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
 
-  TMP_STORAGE_DIR="${TMP_STORAGE_DIR}/extensions"
+  TMP_STORAGE_DIR=${TMP_STORAGE_DIR}/extensions
   rm --recursive --force "${TMP_STORAGE_DIR}"
   ln --force --symbolic "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
 done
