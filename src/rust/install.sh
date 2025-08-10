78ee6edd66edb525465fd4436699c4d534136aa9
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-08-10 23:59:13 +0200

    fix: enable libgcc on Alpine for Rust
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- src/rust/install.sh
+++ src/rust/install.sh
@@ -5,7 +5,7 @@ set -e
 . /etc/os-release
 
 if [ "${ID}" = "alpine" ]; then
-  apk add --no-cache bash coreutils gcompat
+  apk add --no-cache bash coreutils gcompat libgcc
 fi
 
 exec /bin/bash "$(dirname "${0}")/install_actual.sh" "${@}"
