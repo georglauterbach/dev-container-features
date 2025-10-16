333866c27339c374e8dd8c4a502ab35aa0a43497
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-10-16 21:28:14 +0200

    feat: finally push Rust feature install code
    This way laying around for way too long
    
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- src/lang-rust/install.sh
+++ src/lang-rust/install.sh
@@ -2,5 +2,64 @@
 
 set -e -u
 
-mkdir --parents /usr/local/share/dev_containers/features/ghcr_io/georglauterbach/lang_rust/lifecycle_hooks
-cp on_create_command.sh /usr/local/share/dev_containers/features/ghcr_io/georglauterbach/lang_rust/lifecycle_hooks/on_create_command.sh
+FEATURE_SHARE_DIR=/usr/local/share/dev_containers/features/ghcr_io/georglauterbach/lang_rust
+readonly FEATURE_SHARE_DIR
+
+log() {
+  printf "%s %-5s: %s\n" "$(date +"%Y-%m-%dT%H:%M:%S.%6N%:z" || :)" "${1:-}" "${2:-}"
+}
+
+parse_dev_container_options() {
+  log 'info' 'Parsing input from options'
+
+  readonly GENERATE_SHELL_COMPLETIONS="${GENERATE_SHELL_COMPLETIONS:?GENERATE_SHELL_COMPLETIONS not set or null}"
+  readonly RUSTUP_DISABLE_AUTO_SELF_UPDATE="${RUSTUP_DISABLE_AUTO_SELF_UPDATE:?RUSTUP_DISABLE_AUTO_SELF_UPDATE not set or null}"
+}
+
+copy_lifecycle_hook_scripts() {
+  mkdir --parents /usr/local/share/dev_containers/features/ghcr_io/georglauterbach/lang_rust/lifecycle_hooks
+  cp on_create_command.sh /usr/local/share/dev_containers/features/ghcr_io/georglauterbach/lang_rust/lifecycle_hooks/on_create_command.sh
+}
+
+rustup_adjustments() {
+  if command -v rustup >/dev/null 2>/dev/null; then
+    log 'info' "'rustup' is installed"
+  else
+    log 'warn' "'rustup' is not installed - not performing actions associated with 'rustup' (shell completions, disabling automatic self-update)"
+    return 0
+  fi
+
+  if [ "${GENERATE_SHELL_COMPLETIONS}" = 'true' ]; then
+    log 'info' 'Generating completions for rustup and Cargo'
+    mkdir  --parents               /usr/share/bash-completion/completions
+    rustup completions bash       >/usr/share/bash-completion/completions/rustup
+    rustup completions bash cargo >/usr/share/bash-completion/completions/cargo
+  else
+    log 'info' 'Not generating completions for rustup and Cargo'
+  fi
+
+  if [ "${RUSTUP_DISABLE_AUTO_SELF_UPDATE}" = 'true' ]; then
+    log 'info' "Disabling rustup's self-update feature when container runs"
+    touch "${FEATURE_SHARE_DIR}/.rustup_disable_auto_self_update"
+  else
+    log 'info' "Not disabling rustup's self-update feature when container runs"
+  fi
+}
+
+copy_lldb_prettifiers() {
+  log 'info' 'Copying prettifier for LLDB'
+  cp "lldb_prettifier.py" "${FEATURE_SHARE_DIR}/"
+  chmod 777 "${FEATURE_SHARE_DIR}/prettifier_for_lldb.py"
+}
+
+main() {
+  parse_dev_container_options
+
+  mkdir --parents "${FEATURE_SHARE_DIR}"
+
+  copy_lifecycle_hook_scripts
+  rustup_adjustments
+  copy_lldb_prettifiers
+}
+
+main "${@}"
--- src/lang-rust/on_create_command.sh
+++ src/lang-rust/on_create_command.sh
@@ -1,3 +1,81 @@
 #! /bin/sh
 
 set -e -u
+
+FEATURE_SHARE_DIR=/usr/local/share/dev_containers/features/ghcr_io/georglauterbach/lang_rust
+readonly FEATURE_SHARE_DIR
+
+update_directory() {
+  ENV_NAME=${1:?"(bug) environment variable name required"}
+  ENV_VALUE=$(eval "echo \"\$${ENV_NAME}\"")
+
+  echo "Adjusting directory '${ENV_NAME}=${ENV_VALUE}'"
+
+  if [ -z "${ENV_VALUE}" ]; then
+    echo "  -> environment variable '${ENV_NAME}' not set, null or empty"
+    return 1
+  fi
+
+  if [ -d "${ENV_VALUE}" ]; then
+    echo "  -> directory already exists"
+  else
+    printf "  -> creating directory: "
+
+    if mkdir --parents "${ENV_VALUE}" 2>/dev/null \
+    || sudo mkdir --parents "${ENV_VALUE}"; then
+      echo 'done'
+    else
+      echo 'failed'
+      return 1
+    fi
+  fi
+
+  if [ "$(stat -c '%u' "${ENV_VALUE}")" -ne "$(id -u)" ] \
+  || {
+    # we check for Rustup's settings.toml here explicitly because
+    # this file is known to make problems
+    [ -f "${ENV_VALUE}/settings.toml" ] \
+    && [ "$(stat -c '%u' "${ENV_VALUE}/settings.toml")" -ne "$(id -u)" ]
+  }; then
+    printf "  -> updating permissions: "
+
+    if chown --recursive "$(id -u):$(id -g)" "${ENV_VALUE}" 2>/dev/null \
+    || sudo chown --recursive "$(id -u):$(id -g)" "${ENV_VALUE}"; then
+      echo 'done'
+    else
+      echo 'failed'
+      return 1
+    fi
+  else
+    echo "  -> directory already has proper permissions"
+  fi
+}
+
+rustup_adjustments() {
+  echo "Adjusting 'rustup'"
+  if ! command -v rustup >/dev/null 2>/dev/null; then
+    echo "  -> 'rustup' is not installed"
+    echo "  -> not running other commands"
+    return 0
+  fi
+
+  echo "  -> 'rustup' is installed"
+  if [ -f "${FEATURE_SHARE_DIR}/.rustup_disable_auto_self_update" ]; then
+    echo "  -> disabling rustup's self-update feature"
+    rustup set auto-self-update disable
+  else
+    echo "  -> not disabling rustup's self-update feature"
+  fi
+}
+
+main() {
+  for ENV_NAME in 'RUSTUP_HOME' 'CARGO_HOME'; do
+    if ! update_directory "${ENV_NAME}"; then
+      echo "  -> not running other commands"
+    fi
+  done
+
+  rustup_adjustments
+}
+
+main "${@}"
