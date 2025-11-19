4b510ab91a417b540661ccf2304f47d3a06f99ac
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2025-11-19 14:22:22 +0100

    chore: update feature `hermes`
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- src/hermes/install.sh
+++ src/hermes/install.sh
@@ -10,43 +10,15 @@ log() {
 
 parse_dev_container_options() {
   log 'info' 'Parsing input from options'
-
-  readonly INIT_BASHRC="${INIT_BASHRC:?INIT_BASHRC not set or null}"
-  readonly INIT_BASHRC_OVERWRITE="${INIT_BASHRC_OVERWRITE:?INIT_BASHRC_OVERWRITE not set or null}"
-}
-
-initialize_bashrc() {
-  if [ "${INIT_BASHRC}" = 'false' ]; then
-    log 'info' 'Not initializing hermes'
-    return 0
-  fi
-
-  if [ "${INIT_BASHRC_OVERWRITE}" = 'true' ]; then
-    log 'info' 'Initializing hermes by overwriting .bashrc'
-    cat >"${_REMOTE_USER_HOME}/.bashrc" <<"EOF"
-#! /usr/bin/env bash
-
-if [[ -f ${HOME}/.config/bash/90-hermes.sh ]]; then
-  source "${HOME}/.config/bash/90-hermes.sh"
-fi
-EOF
-  else
-    log 'info' 'Initializing hermes by extending .bashrc'
-    cat >>"${_REMOTE_USER_HOME}/.bashrc" <<"EOF"
-
-if [[ -f ${HOME}/.config/bash/90-hermes.sh ]]; then
-  source "${HOME}/.config/bash/90-hermes.sh"
-fi
-EOF
-  fi
 }
 
 main() {
   parse_dev_container_options
-  initialize_bashrc
+  echo "Add '[[ -f ${HOME}/.config/bash/90-hermes.sh ]] && source \"${HOME}/.config/bash/90-hermes.sh\"' to your Bash setup to enable hermes"
 
   mkdir --parents /usr/local/bin/
-  mv "hermes-v11.1.2-$(uname --machine)-unknown-linux-musl" /usr/local/bin/hermes
+  mv "hermes-v11.2.3-$(uname --machine)-unknown-linux-musl" /usr/local/bin/hermes
+  chmod +x /usr/local/bin/hermes
 }
 
 main "${@}"
