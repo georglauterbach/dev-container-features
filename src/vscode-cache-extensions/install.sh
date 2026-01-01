bf4fd22abfe996d0a336c1ebc0828bc61e3929f2
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2026-01-01 16:50:24 +0100

    chore: update Rust LLDB extension & make extension caching work on Alpine
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- src/vscode-cache-extensions/install.sh
+++ src/vscode-cache-extensions/install.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+# This script uses the short flags (e.g., "-f") for commands instead of the
+# long flags (e.g. "--force") to work on Alpine and other distributions.
+
 # shellcheck disable=SC2154
 
 set -e -u
@@ -12,20 +15,20 @@ if [ -z "${_REMOTE_USER_HOME:-}" ]; then
 fi
 
 # We create the mount point and temporary directories here with the
-# correct permissions. This is especially imperative for the mount
-# point as the volume mount will cary over the _permissions_ of an
-# existing directory. We _cannot_ use `chown` here because of
+# correct permissions. This is imperative for the mount point as
+# the volume mount will cary over the _permissions_ of an existing
+# directory. We _cannot_ use `chown` here because of
 # `updateRemoteUserUID: true` in some cases, which would result in
 # a UID mismatch; hence, we need `777` as the permissions.
 for LOOP_VAR in "stable," "insiders,-insiders"; do
-  PERSISTENCE_DIR="${CACHE_MOUNT_POINT}/$(printf '%s' "${LOOP_VAR}" | cut --delimiter=, --fields=1)"
+  PERSISTENCE_DIR="${CACHE_MOUNT_POINT}/$(printf '%s' "${LOOP_VAR}" | cut -d , - 1)"
   TMP_STORAGE_DIR="${_REMOTE_USER_HOME}/.vscode-server$(printf '%s' "${LOOP_VAR}" | cut --delimiter=, --fields=2)"
 
-  mkdir --parents "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
-  chmod --recursive 777 "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
+  # shellcheck disable=SC2174
+  mkdir -m 777 -p "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
 
   TMP_STORAGE_DIR=${TMP_STORAGE_DIR}/extensions
 
-  rm --recursive --force "${TMP_STORAGE_DIR}"
-  ln --symbolic "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
+  rm -r -f "${TMP_STORAGE_DIR}"
+  ln -s "${PERSISTENCE_DIR}" "${TMP_STORAGE_DIR}"
 done
