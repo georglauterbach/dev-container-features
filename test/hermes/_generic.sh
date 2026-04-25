ac9f33987cd43fc2b0a5e5871ee40cd2b79e3122
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2026-04-25 22:37:09 +0200

    feat: update `hermes`
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- test/hermes/_generic.sh
+++ test/hermes/_generic.sh
@@ -3,7 +3,7 @@
 . ./_harness.sh
 
 assert_success 'program exists'              command -v hermes
-assert_success 'program version is correct'  test "$(hermes --version)" = 'hermes 12.4.0'
+assert_success 'program version is correct'  test "$(hermes --version)" = 'hermes 13.0.0'
 assert_success 'program runs'                hermes
 
 finish
